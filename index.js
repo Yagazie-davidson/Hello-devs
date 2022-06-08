52f8291f4474c7fadb73628c051e26eadef972a5
Letam <davidsonletam@gmail.com>
2022-06-08 09:28:02 +0100

    I added the button feature

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function fetchText() {
     quoteText.innerHTML = '"' + data.en + '"'
     author.textContent = data.author
 }
-fetchText();
+
 
 //{id: '5a9b0b752bad9600044b6fcb', author: 'Danny Hillis', en: 'Computers are the most complex objects we human be…n a fundamental sense they are remarkably simple.'}
 
